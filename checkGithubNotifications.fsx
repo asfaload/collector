c0ebae5e17ebfe547b185f7479340dd136aca2ca
Bauduin Raphaël <raphael@asfaload.com>
2024-09-30 15:08:37 +0200

    feat(ghnotif): use lastModified from previous request

--- checkGithubNotifications.fsx
+++ checkGithubNotifications.fsx
@@ -8,7 +8,7 @@ open FsHttp
 open FSharp.Data
 
 
-let getNotifications () =
+let getNotifications (lastModified: DateTimeOffset option) =
     async {
 
         let! response =
@@ -18,7 +18,14 @@ let getNotifications () =
                 UserAgent "rbauduin-test"
                 AuthorizationBearer(Environment.GetEnvironmentVariable("GITHUB_TOKEN"))
                 header "X-GitHub-Api-Version" "2022-11-28"
-            //header "If-Modified-Since" "Mon, 30 Sep 2024 09:21:13 GMT"
+                //header "If-Modified-Since" "Mon, 30 Sep 2024 09:21:13 GMT"
+                header
+                    "If-Modified-Since"
+
+                    (lastModified
+                     |> Option.map (fun offset -> offset.DateTime |> HttpRequestHeaders.IfModifiedSince)
+                     |> Option.map (fun (_h, v) -> v)
+                     |> Option.defaultValue (DateTime.Parse("2020-01-01") |> HttpRequestHeaders.IfModifiedSince |> snd))
             }
             |> Request.sendAsync
 
@@ -27,15 +34,22 @@ let getNotifications () =
         let pollInterval = headers.GetValues("X-Poll-Interval") |> Seq.tryHead
         printfn "pollInterval = %A" pollInterval
 
-        let lastModified =
-            response.content.Headers.LastModified
-            |> (fun n -> if n.HasValue then (Some n.Value) else None)
+        if response.statusCode = Net.HttpStatusCode.NotModified then
+            printfn "Not modified"
+            return pollInterval, lastModified, System.Text.Json.JsonDocument.Parse("[]")
+        else if response.statusCode = Net.HttpStatusCode.OK then
+            let lastModified =
+                response.content.Headers.LastModified
+                |> (fun n -> if n.HasValue then (Some n.Value) else None)
 
-        printfn "lastModified = %A" lastModified
-        let s = response |> Response.toText
-        File.WriteAllText("notifications.json", s)
-        let json = System.Text.Json.JsonDocument.Parse(s)
-        return pollInterval, lastModified, json
+            printfn "lastModified = %A" lastModified
+            let s = response |> Response.toText
+            File.WriteAllText("notifications.json", s)
+            let json = System.Text.Json.JsonDocument.Parse(s)
+            return pollInterval, lastModified, json
+        else
+            failwithf "Unexpected response status code %A" (response.statusCode)
+            return pollInterval, lastModified, System.Text.Json.JsonDocument.Parse("[]")
     }
 
 let getNotificationsFromDisk () =
@@ -51,7 +65,7 @@ let getNotificationsFromDisk () =
 let main () =
     async {
         //let! (Some 60, None, json) = getNotificationsFromDisk ()
-        let! pollInterval, lastModified, json = getNotifications ()
+        let! pollInterval, lastModified, json = getNotifications (None)
 
         let nextPollAt =
             pollInterval
@@ -62,6 +76,9 @@ let main () =
         let user = (json.RootElement[0]?repository?owner?login.ToString())
         let repo = (json.RootElement[0]?repository?name.ToString())
         printfn "New release for %s/%s" user repo
+
+        do! Async.Sleep(TimeSpan.FromSeconds(float (pollInterval |> Option.defaultValue "60")))
+        let! pollInterval, lastModified, json = getNotifications (lastModified)
         return 0
 
     }
