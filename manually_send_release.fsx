aa7859fb2d77ca574f2cdfb762fa9f8b0dc25622
Bauduin Raphaël <raphael@asfaload.com>
2024-10-03 09:09:22 +0200

    add: manually trigger release collection

---
+++ manually_send_release.fsx
@@ -0,0 +1,28 @@
+// Script to manually trigger a release collection
+// dotnet fsi manually_send_release.fsx $user $repo
+#r "nuget: DiskQueue, 1.7.1"
+#load "lib/Shared.fsx"
+
+open System
+open DiskQueue
+open Asfaload.Collector
+open System.Text.Json
+
+let queue = Environment.GetEnvironmentVariable("RELEASES_QUEUE")
+let releasingReposQueue = PersistentQueue.WaitFor(queue, TimeSpan.FromSeconds(3))
+let qSession = releasingReposQueue.OpenSession()
+
+let args = fsi.CommandLineArgs
+let user = args[1]
+let repoName = args[2]
+
+let repo =
+    { user = user
+      repo = repoName
+      kind = Github
+      checksums = [] }
+
+qSession.Enqueue(repo |> JsonSerializer.Serialize |> System.Text.Encoding.ASCII.GetBytes)
+qSession.Flush()
+qSession.Dispose()
+releasingReposQueue.Dispose()
