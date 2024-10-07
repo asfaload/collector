b94146e460b64813d004055c83a533d84775343a
Bauduin Raphaël <raphael@asfaload.com>
2024-10-07 12:39:06 +0200

    refactor: move queue code to dedicated file
    Otherwise it required to load DiskQueue in all script that were using
    Shared, even when not handling queues.

---
+++ lib/Queue.fsx
@@ -0,0 +1,24 @@
+#r "nuget: DiskQueue, 1.7.1"
+#load "Shared.fsx"
+namespace Asfaload.Collector
+
+module Queue =
+    open System
+    open DiskQueue
+    open System.Text.Json
+
+    let triggerReleaseDownload (user: string) (repo: string) =
+        let queue = Environment.GetEnvironmentVariable("RELEASES_QUEUE")
+        let releasingReposQueue = PersistentQueue.WaitFor(queue, TimeSpan.FromSeconds(3))
+        let qSession = releasingReposQueue.OpenSession()
+
+        let repo =
+            { user = user
+              repo = repo
+              kind = Github
+              checksums = [] }
+
+        qSession.Enqueue(repo |> JsonSerializer.Serialize |> System.Text.Encoding.ASCII.GetBytes)
+        qSession.Flush()
+        qSession.Dispose()
+        releasingReposQueue.Dispose()
--- lib/Shared.fsx
+++ lib/Shared.fsx
@@ -1,5 +1,4 @@
 #r "nuget: FSharp.SystemTextJson, 1.3.13"
-#r "nuget: DiskQueue, 1.7.1"
 
 namespace Asfaload.Collector
 
@@ -16,24 +15,3 @@ type Repo =
       user: string
       repo: string
       checksums: string list }
-
-module Queue =
-    open System
-    open DiskQueue
-    open System.Text.Json
-
-    let triggerReleaseDownload (user: string) (repo: string) =
-        let queue = Environment.GetEnvironmentVariable("RELEASES_QUEUE")
-        let releasingReposQueue = PersistentQueue.WaitFor(queue, TimeSpan.FromSeconds(3))
-        let qSession = releasingReposQueue.OpenSession()
-
-        let repo =
-            { user = user
-              repo = repo
-              kind = Github
-              checksums = [] }
-
-        qSession.Enqueue(repo |> JsonSerializer.Serialize |> System.Text.Encoding.ASCII.GetBytes)
-        qSession.Flush()
-        qSession.Dispose()
-        releasingReposQueue.Dispose()
