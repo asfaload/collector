0d3edf211644fd0e594d19e1686cdc0c3c46c0bc
Bauduin Raphaël <raphael@asfaload.com>
2024-10-23 12:23:50 +0200

    feat(index): generate json

--- generateIndex.fsx
+++ generateIndex.fsx
@@ -1,23 +1,28 @@
+#r "nuget: FSharp.SystemTextJson, 1.3.13"
+
 open System
 open System.IO
+open System.Text.Json
 open System.Text.RegularExpressions
+
+open System.Text.Json.Serialization
 
 type Algo =
     | Sha256
     | Sha512
 
-type Checksum =
-    { algo: Algo
+type FileChecksum =
+    { fileName: string
+      algo: Algo
       source: string
       value: string }
 
-type fileChecksum = Map<string, Checksum>
-type filesChecksums = seq<fileChecksum>
+type FilesChecksums = seq<FileChecksum>
 
 type IndexFile =
-    { mirroredOn: DateTime
-      publishedOn: DateTime
-      publishedFiles: filesChecksums }
+    { mirroredOn: DateTime option
+      publishedOn: DateTime option
+      publishedFiles: FilesChecksums }
 
 let rec getLeafDirectories (root: string) =
     seq {
@@ -37,10 +42,14 @@ let rec getLeafDirectories (root: string) =
                         yield leaf
     }
 
-let handleChecksumFile (path: string) : seq<fileChecksum> =
+let handleChecksumFile (path: string) : FilesChecksums =
     File.ReadLines path
-    |> Seq.map (fun line ->
-        let parts = line.Split(" ")
+    |> Seq.map (fun line -> line.Split(" ") |> Array.filter (fun e -> e <> ""))
+    |> Seq.filter (fun a ->
+
+        let cdt = a <> [| "-----BEGIN"; "PGP"; "SIGNED"; "MESSAGE-----" |]
+        cdt)
+    |> Seq.map (fun parts ->
 
         let checksum, file =
             match parts with
@@ -50,7 +59,9 @@ let handleChecksumFile (path: string) : seq<fileChecksum> =
                     (sha, regex.Replace(fileName, "", 1).Trim())
                 else
                     (sha, fileName.Trim())
-            | _ -> failwithf "error handling line\n%s\n from file %s" line path
+            | other ->
+                printfn "%A" other
+                failwithf "error handling line parts\n%A\n from file %s" parts path
 
         let algo =
             match checksum.Length with
@@ -58,32 +69,45 @@ let handleChecksumFile (path: string) : seq<fileChecksum> =
             | 128 -> Sha512
             | _ -> failwithf "In file %s,\n unknown checksum algo for checksum value:\n%s" path checksum
 
-        let checksumRecord =
-            { algo = algo
-              source = FileInfo(path).Name
-              value = checksum }
+        { fileName = file
+          algo = algo
+          source = (FileInfo(path).Name)
+          value = checksum }
 
-        Map.empty.Add(file, checksumRecord)
 
     )
 
 
 let handleChecksumsFilesInLeaf (leafDir: string) =
-    Directory.EnumerateFiles leafDir
-    |> Seq.map (fun checksumFile -> handleChecksumFile checksumFile)
+    let checksums =
+        Directory.EnumerateFiles leafDir
+        // Filter out PGP signature files
+        |> Seq.filter (fun f ->
+            let firstLine = File.ReadLines f |> Seq.head
+            firstLine <> "-----BEGIN PGP SIGNED MESSAGE-----")
+        |> Seq.map (fun checksumFile -> handleChecksumFile checksumFile)
+        // Merge all infor from all checksums files in one Seq
+        |> Seq.concat
+
+    { mirroredOn = None
+      publishedOn = None
+      publishedFiles = checksums }
 
 let generateChecksumsList (rootDir: string) =
     getLeafDirectories rootDir
-    |> Seq.map (fun leafDir -> handleChecksumsFilesInLeaf leafDir
-
-
-    )
+    |> Seq.map (fun leafDir ->
+        let checksumsInfo = handleChecksumsFilesInLeaf leafDir
+        let options = JsonFSharpOptions.Default().ToJsonSerializerOptions()
+        let json = JsonSerializer.Serialize(checksumsInfo, options)
+        printfn "********************* %s *********************" leafDir
+        printfn "%s" json)
+    |> Seq.iter (fun _ -> ())
 
 
 let main () =
     let gitDir = System.Environment.GetEnvironmentVariable("BASE_DIR")
-    getLeafDirectories gitDir |> Seq.iter (fun leaf -> printfn "%s" leaf)
+    //getLeafDirectories gitDir |> Seq.iter (fun leaf -> printfn "%s" leaf)
+    generateChecksumsList gitDir
 
 
-main ()
 main ()
