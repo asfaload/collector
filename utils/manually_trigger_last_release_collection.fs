9a34589d5a51084b994253fee5377aad2222674e
Bauduin Raphaël <raphael@asfaload.com>
2024-11-05 15:45:20 +0100

    build: migrate one util script to compiled

---
+++ utils/manually_trigger_last_release_collection.fs
@@ -0,0 +1,21 @@
+// Utility to manually trigger a release collection
+// manually_send_release.fsx $user $repo
+
+open Asfaload.Collector
+
+
+let args = System.Environment.GetCommandLineArgs()
+let user = args[1]
+let repoName = args[2]
+
+printfn "triggering for %s/%s, Ok?" user repoName
+printfn "press enter to continue, Ctrl-C to interrupt"
+System.Console.ReadLine() |> ignore
+
+let repo =
+    { user = user
+      repo = repoName
+      kind = Github
+      checksums = [] }
+
+Queue.publish repo |> Async.AwaitTask |> Async.RunSynchronously
