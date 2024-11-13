38635db0e647c344cf5ed96aac865e19454fa655
Bauduin Raphaël <raphael@asfaload.com>
2024-11-13 15:22:21 +0100

    feat: add jwtjs for jwt validation

---
+++ jwtjs/index.ts
@@ -0,0 +1,24 @@
+import { createRemoteJWKSet, jwtVerify } from "jose";
+
+export async function validateToken(
+  token: string,
+): Promise<Record<string, unknown>> {
+  const jwks = createRemoteJWKSet(
+    new URL("https://token.actions.githubusercontent.com/.well-known/jwks"),
+  );
+  const { payload } = await jwtVerify(token, jwks, {
+    issuer: "https://token.actions.githubusercontent.com",
+  });
+  return payload;
+}
+
+if (Bun.argv.length < 3) {
+  console.log("Pass jwt as argument to script")
+  process.exit(1);
+}
+//let token = await Bun.file("token").text()
+let token = Bun.argv[2];
+
+let result = await validateToken(token);
+
+console.log(result.repository)
