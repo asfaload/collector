635738e7b04330d4e415bd4bd002bced3029e706
Bauduin Raphaël <raphael@asfaload.com>
2025-02-06 14:46:51 +0100

    test: single test of getPollInterval

--- tests/githubNotifications/GithubNotifications.fs
+++ tests/githubNotifications/GithubNotifications.fs
@@ -1,5 +1,16 @@
 module tests.GithubNotifications
 
+open FsHttp
+open FsHttp.Tests.TestHelper
+open FsHttp.Tests.Server
+
+open Suave
+open Suave.Operators
+open Suave.Filters
+open Suave.Writers
+open Suave.Successful
+open GithubNotifications
+
 open NUnit.Framework
 open FsUnit
 
@@ -7,4 +18,11 @@ open FsUnit
 let Setup () = ()
 
 [<Test>]
-let Test1 () = true |> should equal true
+let test_GetPollInterval () =
+    let baseRoute = GET >=> request (fun r -> "body content" |> OK)
+    use server = baseRoute >=> setHeader "X-Poll-Interval" "30" |> serve
+
+    let response = http { GET(url "") } |> Request.send
+
+    let pollInterval = getPollInterval response.headers
+    pollInterval |> should equal (Some "30")
