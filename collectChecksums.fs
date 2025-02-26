7a17190e25de5b26babd84b617895a0d0b654a44
Bauduin Raphaël <raphael@asfaload.com>
2025-02-26 09:03:02 +0100

    fix: do not collect checksums if it was done in the last 30 minutes
    This is introduced to work around a bug to be fixed in notifications
    retrieval, but effectively limits projects to one release every 30
    minutes which might be useful in itself.

--- collectChecksums.fs
+++ collectChecksums.fs
@@ -9,6 +9,15 @@ open Asfaload.Collector.ChecksumsCollector
 open Asfaload.Collector
 open Asfaload.Collector.Ignore
 
+open System
+
+let mininmumTimeBetweenReleases =
+    System.Environment.GetEnvironmentVariable("TIME_BETWEEN_RELEASES_MINUTES")
+    |> Option.ofObj
+    |> Option.map int
+    |> Option.defaultValue 30
+
+let mutable reposSeen: Map<string, DateTimeOffset> = Map.empty
 
 let rec readQueue () =
     async {
@@ -24,10 +33,23 @@ let rec readQueue () =
         do!
             Queue.consumeRepoReleases (fun repo ->
                 printfn "user = %s , repo = %s" repo.user repo.repo
+                let mapKey = $"{repo.user}/{repo.repo}"
 
-                if isGithubIgnored repo.user repo.repo then
-                    printfn "Not collecting checksums of ignored repo %s/%s" repo.user repo.repo
+                if Map.containsKey mapKey reposSeen then
+                    let lastSeen = Map.find mapKey reposSeen
+
+                    if DateTimeOffset.Now.Subtract(lastSeen) < TimeSpan.FromMinutes(mininmumTimeBetweenReleases) then
+                        printfn
+                            "ignoring repo %s as it was seen in the last %d minutes"
+                            mapKey
+                            mininmumTimeBetweenReleases
+                    else if isGithubIgnored repo.user repo.repo then
+                        printfn "Not collecting checksums of ignored repo %s/%s" repo.user repo.repo
+                    else
+                        reposSeen <- Map.add mapKey DateTimeOffset.Now reposSeen
+                        handleRepoRelease repo |> Async.RunSynchronously
                 else
+                    reposSeen <- Map.add mapKey DateTimeOffset.Now reposSeen
                     handleRepoRelease repo |> Async.RunSynchronously)
             |> Async.AwaitTask
 
