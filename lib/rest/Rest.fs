c08735dca8c289d7a43a57664d358979da50d30d
Bauduin Raphaël <raphael@asfaload.com>
2025-05-10 09:32:10 +0200

    feat: add Repo rest module

--- lib/rest/Rest.fs
+++ lib/rest/Rest.fs
@@ -37,3 +37,31 @@ module Release =
 
             return response.ToText() |> ReleaseData.Parse
         }
+
+module Repo =
+    [<Literal>]
+    let repoSample = __SOURCE_DIRECTORY__ + @"/samples/repo.json"
+
+    type RepoData = JsonProvider<repoSample>
+
+    let getRepo (url: System.Uri) =
+        async {
+
+            let ua =
+                System.Environment.GetEnvironmentVariable("GH_USER_AGENT")
+                |> Option.ofObj
+                |> Option.defaultValue "rbauduin-test"
+
+            let! response =
+                http {
+                    GET url.OriginalString
+                    Accept "application/vnd.github+json"
+                    UserAgent ua
+                    AuthorizationBearer(Environment.GetEnvironmentVariable("GITHUB_TOKEN"))
+                    header "X-GitHub-Api-Version" "2022-11-28"
+                //header "If-Modified-Since" "Mon, 30 Sep 2024 09:21:13 GMT"
+                }
+                |> Request.sendAsync
+
+            return response.ToText() |> RepoData.Parse
+        }
